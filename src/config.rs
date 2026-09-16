659498bfa45922261098b0224d5db8a0e5a3f60c
Alexander Hill <me@alxhill.com>
2026-09-16 11:43:07 -0400

    feat(usage): add a block display with a solid fill over shaded empty cells

--- src/config.rs
+++ src/config.rs
@@ -350,6 +350,8 @@ pub enum UsageDisplay {
     Bar,
     #[serde(alias = "capped_bars", alias = "capped")]
     CappedBar,
+    #[serde(alias = "blocks")]
+    Block,
     #[serde(alias = "sparklines", alias = "spark", alias = "sparks")]
     Sparkline,
     #[serde(alias = "number", alias = "numbers", alias = "num")]
@@ -637,6 +639,8 @@ mod tests {
             ("capped_bar", UsageDisplay::CappedBar),
             ("capped_bars", UsageDisplay::CappedBar),
             ("capped", UsageDisplay::CappedBar),
+            ("block", UsageDisplay::Block),
+            ("blocks", UsageDisplay::Block),
             ("sparkline", UsageDisplay::Sparkline),
             ("sparklines", UsageDisplay::Sparkline),
             ("spark", UsageDisplay::Sparkline),
