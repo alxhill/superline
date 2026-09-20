97d1540e03a1020f829afdd329167445a1045267
Alexander Hill <me@alxhill.com>
2026-09-20 17:30:33 -0400

    fix(os): use the standard padded segment instead of hand-rolled spacing

--- src/modules/os.rs
+++ src/modules/os.rs
@@ -137,8 +137,8 @@ impl<S: OsScheme> Os<S> {
 
 impl<S: OsScheme> Module for Os<S> {
     fn append_segments(&mut self, powerline: &mut Powerline) {
-        powerline.add_short_segment(
-            format!("{} ", S::os_symbol(self.kind)),
+        powerline.add_segment(
+            S::os_symbol(self.kind),
             Style::simple(S::os_fg(), S::os_bg()),
         );
     }
